04a293b8ef264393190887426edb6834c8221062
Tatiana Bondarenko <62190724+bondarenkotatiana96@users.noreply.github.com>
2021-02-12 21:33:11 -0800

    add project files

---
+++ src/EditTodoForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import useInputState from './hooks/useInputState';
+
+function EditTodoForm({ id, task, editTodo, toggleEditForm }) {
+    const [value, handleChange, reset] = useInputState(task);
+    return (
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            editTodo(id, value);
+            reset();
+            toggleEditForm();
+            }}
+            style={{marginLeft: "1rem", width: "100%"}}
+        >
+        <TextField 
+            value={value} 
+            onChange={handleChange} 
+            margin="normal"
+            autoFocus
+        />
+        </form>
+    )
+}
+
+export default EditTodoForm;
---
+++ src/TodoApp.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import useTodoState from './hooks/useTodoState';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import Grid from '@material-ui/core/Grid';
+
+function TodoApp() {
+    const initialTodos = [{id: 1, task: "Pet a Monkey", completed: false}];
+    const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(initialTodos);
+    // const initialTodos = [
+    //     {id: 1, task: "Workout", completed: false},
+    //     {id: 2, task: "Walk the dog", completed: true },
+    //     {id: 3, task: "Make a breakfast", completed: false}
+    // ];
+
+    return (
+    <Paper 
+        style={{
+            padding: 0,
+            margin: 0,
+            height: "100vh",
+            backgroundColor: "#fafafa"
+        }}
+        elevation={0}
+    >
+        <AppBar color="primary" position="static" style={{ height: "64px" }}>
+            <ToolBar>
+                <Typography color="inherit">Todo List with Hooks</Typography>
+            </ToolBar>
+        </AppBar>
+        <Grid container justify="center" style={{marginTop: "1rem"}}>
+        <Grid item xs={11} md={8} lg={4}>
+                <TodoForm addTodo={addTodo} />
+                <TodoList 
+                    todos={todos} 
+                    removeTodo={removeTodo} 
+                    toggleTodo={toggleTodo}
+                    editTodo={editTodo}
+                />
+            </Grid>
+        </Grid>
+    </Paper>
+    )
+};
+
+export default TodoApp;
